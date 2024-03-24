6023445b1dd1c97513c26c902bef075a092e8b8b
Maximous ElKess Ayoub <89272370+maxiayoub@users.noreply.github.com>
2024-03-24 16:40:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Creating VPC
 resource "aws_vpc" "demovpc" {
   cidr_block       = var.vpc_cidr
-  #instance_tenancy = "default"
+  instance_tenancy = "default"
 
   tags = {
     Name = "Lab VPC"
